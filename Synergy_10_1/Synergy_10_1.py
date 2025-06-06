9d571484b600d8be7f9e570c8d14f67a5022bd7e
Zerov Denis <dinozer1234@gmail.com>
2025-06-06 18:20:36 +0300

    Add project files.

---
+++ Synergy_10_1/Synergy_10_1.py
@@ -0,0 +1,14 @@
+﻿pets = {}
+
+pet_name = input("Введите имя питомца: ")
+pet_type = input("Введите вид питомца: ")
+pet_age = int(input("Введите возраст питомца: "))
+owner_name = input("Введите имя владельца: ")
+
+pets[pet_name] = {
+    'Вид питомца': pet_type,
+    'Возраст питомца': pet_age,
+    'Имя владельца': owner_name
+}
+
+print(pets)
